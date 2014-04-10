233f9b90aa9cceafb5c41b71f17a0a91ba80400c
Valera19900 <kaktuz34@gmail.com>
2014-04-10 14:04:07 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,72 @@
 $(document).ready(function() {
 
+	// $('.js_loginform').validate({
+	// 	rules:{
+	// 		name:{
+	// 			required: true,
+	// 		},
+	// 		phone:{
+	// 			required: true,
+	// 		},
+	// 	},
+	// });
+
+	// $('.js_loginform1').validate({
+	// 	rules:{
+	// 		name:{
+	// 			required: true,
+	// 		},
+	// 		phone:{
+	// 			required: true,
+	// 		},
+	// 	},
+	// });
+
+	$('.js_loginform').each(function() {
+
+    $(this).validate({
+	 
+			rules:{
+
+				name:{
+						required: true,
+				},
+				
+			  phone:{
+			      required: true,
+			  },
+
+			},
+
+    });
+
+	});
+
 });
+
+$('.fancybox').fancybox({
+	helpers: {
+		overlay: {
+			locked: false
+		}
+	}
+});
+
+
+function js_numbers(input) { 
+  var value = input.value; 
+  var rep = /[-\.;":'a-zA-Zа-яА-Я]/; 
+  if (rep.test(value)) { 
+      value = value.replace(rep, ''); 
+      input.value = value; 
+  } 
+}
+
+function js_name(input) { 
+    var value = input.value; 
+    var rep = /[a-zA-Zа]/; 
+    if (rep.test(value)) { 
+        value = value.replace(rep, ''); 
+        input.value = value; 
+    } 
+	}
